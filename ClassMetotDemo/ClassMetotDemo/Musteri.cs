ad81e242975a92d74aa633d22101d31592440bef
Hakan Uğurdil <65564279+S1mpleF@users.noreply.github.com>
2021-06-22 16:42:25 +0300

    Add files via upload

---
+++ ClassMetotDemo/ClassMetotDemo/Musteri.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class Musteri
+    {
+        public int id { get; set; }
+
+        public string MusteriAdSoyad { get; set; }
+
+        public int Yas { get; set; }
+
+        public string TcKimlikNo { get; set; }
+
+        public string Adres { get; set; }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi: " + musteri.id + " " + musteri.MusteriAdSoyad);
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi: " + musteri.id + " " + musteri.MusteriAdSoyad);
+        }
+
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Musteri[] musteriler = new Musteri[] { musteri };
+            foreach (Musteri mus in musteriler)
+            {
+                Console.WriteLine("Kayıtlı müşteri listesi: " + mus.id + " " + mus.MusteriAdSoyad + " " + mus.Yas + " " + mus.TcKimlikNo + " " + mus.Adres);
+            }
+        }
+
+    }
+}
