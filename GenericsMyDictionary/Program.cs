7deb1bcb4334b9b5da02478053e7703a8ab3a141
Hakan Uğurdil <65564279+S1mpleF@users.noreply.github.com>
2021-06-23 14:03:05 +0300

    Add files via upload

---
+++ GenericsMyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> MyDictionary1 = new MyDictionary<int, string>();
+            MyDictionary1.Add(22, "Hakan");
+            MyDictionary1.Add(01, "Adana");
+            MyDictionary1.Add(33, "Mersin");
+            Console.WriteLine(MyDictionary1.Count);
+            Console.ReadLine();
+        }
+    }
+}
